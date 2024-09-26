14f6e040892a0858963325975a8b64b471ce71b7
BrunoAlan <alan.brunoab@gmail.com>
2024-09-26 09:16:53 -0300

    Refactor App component to use useRandom hook for fetching and displaying a random number

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 import './App.css';
+import { useRandom } from './hooks/useRandom';
 
 // https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new
 
 function App() {
 
-  const getCriptoNumber = async () => {
-    const response = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new');
-    const data = await response.json();
-    return Number(data);
-  };
+  const { error, isFetching, isLoading, refetch, number } = useRandom();
 
-  const {
-    isLoading,
-    isFetching,
-    data: number,
-    refetch } = useQuery({
-      queryKey: ['randomNumber'],
-      queryFn: getCriptoNumber
-    });
 
   return (
     <>
       {
         isFetching ? <h1>Loading...</h1> : <h1>Number: {number}</h1>
+      }
+      {
+        error && <h1>Error: {error}</h1>
       }
       <div>...</div>
 
